3753fc0559c1723db388cf32eb3bb34830ec2ae0
leftrana <dandyelf32@gmail.com>
2024-01-14 18:42:06 +0300

    struct add

--- day01/read_db/read_db.go
+++ day01/read_db/read_db.go
@@ -1,5 +1,17 @@
 package readdb
 
+type DataBase struct {
+	Cake []struct {
+		Name        string `json:"name"`
+		Time        string `json:"time"`
+		Ingredients []struct {
+			IngredientName  string `json:"ingredient_name"`
+			IngredientCount string `json:"ingredient_count"`
+			IngredientUnit  string `json:"ingredient_unit,omitempty"`
+		} `json:"ingredients"`
+	} `json:"cake"`
+}
+
 func readDb() {
 
 }
