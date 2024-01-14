4d55059ae97be18bd2c753a013b19c272437f210
leftrana <dandyelf32@gmail.com>
2024-01-14 20:59:12 +0300

    day01 ex00 add

--- day01/read_db/read_db.go
+++ day01/read_db/read_db.go
@@ -1,17 +1,48 @@
 package readdb
 
-type DataBase struct {
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Recipes struct {
 	Cake []struct {
-		Name        string `json:"name"`
-		Time        string `json:"time"`
+		Name        string `json:"name,omitempty"`
+		Time        string `json:"time,omitempty"`
 		Ingredients []struct {
-			IngredientName  string `json:"ingredient_name"`
-			IngredientCount string `json:"ingredient_count"`
+			IngredientName  string `json:"ingredient_name,omitempty"`
+			IngredientCount string `json:"ingredient_count,omitempty"`
 			IngredientUnit  string `json:"ingredient_unit,omitempty"`
-		} `json:"ingredients"`
-	} `json:"cake"`
+		} `json:"ingredients,omitempty"`
+	} `json:"cake,omitempty"`
 }
 
-func readDb() {
+func (r *Recipes) RecipesRead {
 
+}
+
+func readDb(file_name string) {
+	jsonFile, err := readFile(file_name)
+
+	var json_base RecipesJson
+
+	json.Unmarshal(jsonFile, &json_base)
+	for i := 0; i < len(json_base.Cake); i++ {
+		fmt.Println("User Type: " + json_base.Cake[i].Name)
+		fmt.Println("User Age: " + json_base.Cake[i].Time)
+		fmt.Println("User Name: " + json_base.Cake[i].Ingredients[i].IngredientName)
+		fmt.Println("Facebook Url: " + json_base.Cake[i].Ingredients[i].IngredientUnit)
+	}
+
+}
+
+func readFile(file_name string) ([]byte, error) {
+	file, err := os.ReadFile(file_name)
+	if err != nil {
+		return nil, fmt.Errorf("open file error: %w", err)
+	}
+
+	return file, nil
 }
