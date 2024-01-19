6b6b9bde01dae8b832d22d5385f249d39a78ebfd
leftrana <dandyelf32@google.com>
2024-01-19 14:29:41 +0300

    interface add

--- day01/ex00/ex00.go
+++ day01/ex00/ex00.go
@@ -6,16 +6,50 @@ import (
 	"fmt"
 )
 
+// example start
+type XmlDb struct {
+	filename string
+}
+
+func (b XmlDb) ReadDb() (db readdb.Recipes, err error) {
+	return
+}
+
+type JsonDb struct {
+	filename string
+}
+
+func (c JsonDb) ReadDb() (db readdb.Recipes, err error) {
+	return
+}
+
+type DBReader interface {
+	ReadDb() (db readdb.Recipes, err error)
+}
+
+func WriteLog(s DBReader) (db readdb.Recipes, err error) {
+	return s.ReadDb()
+}
+
+// example end
+
 func main() {
+
 	filename := flag.String("f", "", "json or xml file")
 	flag.Parse()
-	// fmt.Println(*filename)
 
-	db, err, format := readdb.ParsFile(*filename)
+	xml := XmlDb{*filename}
+	db, err := WriteLog(xml)
+
+	// Инициализируем объект Count и передаём в WriteLog().
+	json := JsonDb{*filename}
+	db, err = WriteLog(json)
+	// fmt.Println(*filename)
 	if err != nil {
 		fmt.Println(err.Error())
 
 		return
 	}
-	readdb.PrintRecipes(db, format)
+	fmt.Println(db)
+	// readdb.PrintRecipes(db, format)
 }
